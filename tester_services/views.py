31f78ae93e48b6fa82c65838d20a27e9492799b3
alenamedzova <alena.medzova@gmail.com>
2023-01-03 21:30:04 +0100

    user management added

--- tester_services/views.py
+++ tester_services/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     if request.user.is_authenticated:
-        status = "You're logged in."
+        status = "You're currently logged in."
     else:
-        status = "You're not logged in."
-    context = {'Your Status:': status}
+        status = "You're not currently logged in."
+    context = {'status': status}
     return render(request, "tester_services/home.html", context)
