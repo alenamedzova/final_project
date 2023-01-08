b80f3b5b57239f77ad651123ee2f6eb77a47d34b
Ivan Medzo <medzo@labyrint.sk>
2023-01-08 17:47:42 +0100

    registrovanie modelov

--- test_generator/admin.py
+++ test_generator/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from test_generator.models import *
 
 # Register your models here.
+admin.site.register(Themes)
+admin.site.register(TestQuestions)
+admin.site.register(TestAnswers)
+admin.site.register(GTest)
+admin.site.register(GTestAnswers)
